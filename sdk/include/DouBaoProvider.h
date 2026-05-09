c3a89fc060cf7570f232e50f641da7c18266bc1d
Kismet0818 <183368639+Kismet0818@users.noreply.github.com>
2026-05-09 21:39:54 +0800

    修改bug

--- sdk/include/DouBaoProvider.h
+++ sdk/include/DouBaoProvider.h
@@ -26,5 +26,7 @@ namespace ai_chat_sdk{
         virtual std::string sendMessageStream(const std::vector<Message>& messages, 
                                               const std::map<std::string, std::string>& requestParam,
                                               std::function<void(const std::string&, bool)> callback);   // callback: 对模型返回的增量数据如何处理，第一个参数为增量数据，第二个参数为是否为最后一个增量数据
+    private:
+        std::string _modelName;
     };
 }
